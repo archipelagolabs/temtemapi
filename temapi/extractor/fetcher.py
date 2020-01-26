77b9dd22d653c10605ffe7a7491540b046a254a6
João Vitor Maia <joaocampo2102@gmail.com>
2020-01-26 18:08:49 -0300

    Extracts and saves Techniques

--- temapi/extractor/fetcher.py
+++ temapi/extractor/fetcher.py
@@ -3,7 +3,7 @@ import json
 import requests
 from parsel import Selector
 
-from temapi.commons.models import Temtem
+from temapi.commons.models import Temtem, Technique
 from temapi.commons.paths import OUTPUTS_DIR
 from temapi.extractor import extractors
 
@@ -57,6 +57,63 @@ def fetch_temtem(name):
         cry=data.get('Cry'),
     )
 
+technique_extractors_map = {
+    'Link': extractors.extract_technique_link,
+    'Name': extractors.extract_technique_name,
+    'Type': extractors.extract_technique_type,
+    'Class': extractors.extract_technique_class,
+    'Damage': extractors.extract_technique_damage,
+    'Stamina Cost': extractors.extract_technique_stamina,
+    'Hold': extractors.extract_technique_hold,
+    'Priority': extractors.extract_technique_priority,
+    'Synergy': extractors.extract_technique_synergy,
+    'Synergy Effect': extractors.extract_technique_synergy_effect,
+    'Targets': extractors.extract_technique_targets
+}
+
+def fetch_techniques_links():
+    print('Getting techniques')
+    response = requests.get("https://temtem.gamepedia.com/Techniques")
+
+    sel = Selector(text=response.text)
+    infos = sel.css('table.wikitable > tbody > tr')
+    infos.pop(0)
+
+    return [
+        technique_extractors_map['Link'](info.css('td')[0])
+        for info in infos
+    ]
+
+
+def fetch_techniques(link : str):
+    response = requests.get(f"https://temtem.gamepedia.com{link}")
+
+    sel = Selector(text=response.text)
+    infos = sel.css('table.infobox-table > tbody > tr.infobox-row')
+
+    keys = infos.css('th.infobox-row-name > b').xpath('text()').getall()
+
+    data = {}
+    data['Name'] = link.replace('_', ' ').replace('/', '')
+    data['Description'] = ''.join(sel.xpath('//*[@id="mw-content-text"]/div/p[2]/i/text()').getall())
+
+    for key, csel in zip(keys, infos):
+        data[key] = technique_extractors_map[key](csel.css('.infobox-row-value'))
+
+    return Technique(
+        name=data['Name'],
+        description=data['Description'],
+        type=data['Type'],
+        category=data['Class'],
+        damage=data['Damage'],
+        stamina_cost=data['Stamina Cost'],
+        hold=data.get('Hold'),
+        priority=data['Priority'],
+        targets=data['Targets'],
+        synergy=data.get('Synergy'),
+        synergy_effect=data.get('Synergy Effect')
+    )
+
 
 def fetch_traits():
     print(f'Getting traits')
@@ -78,9 +135,11 @@ def save(entities, filename):
 def run():
     names = fetch_temtem_name_list()
     temtems = [fetch_temtem(name) for name in names]
-    for t in temtems:
-        print(t)
     save(temtems, 'temtems.json')
 
     traits = fetch_traits()
     save(traits, 'traits.json')
+
+    links = fetch_techniques_links()
+    techniques = [fetch_techniques(link) for link in links]
+    save(techniques, 'techniques.json')
