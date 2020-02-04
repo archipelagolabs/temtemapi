4f0676ef002b04fa4d7af6157258dd8135b74471
Gustavo Emanuel Kundlatsch <gustavo.kundlatsch@gmail.com>
2020-02-04 00:25:18 -0300

    Add items endpoints (#27)
    Co-authored-by: Caio Pereira Oliveira <caiopoliveira@gmail.com>

--- temapi/api/routes/__init__.py
+++ temapi/api/routes/__init__.py
@@ -1,9 +1,11 @@
 from .techniques import router as techniques_router
 from .temtems import router as temtem_router
 from .traits import router as traits_router
+from .items import router as items_router
 
 routers = [
     ('/temtems', temtem_router),
     ('/traits', traits_router),
+    ('/items', items_router),
     ('/techniques', techniques_router),
 ]
---
+++ temapi/api/routes/items.py
@@ -0,0 +1,31 @@
+from typing import List
+
+from fastapi import APIRouter, HTTPException
+
+from temapi.api.loaders.items import ItemLoader
+from temapi.commons.models import Item
+
+router = APIRouter()
+
+item_loader = ItemLoader()
+
+
+@router.get(
+    '/',
+    response_model=List[Item],
+)
+def list_items():
+    return item_loader.items
+
+
+@router.get(
+    '/{name}',
+    response_model=Item,
+)
+def get_item_by_name(name: str):
+    item = item_loader.get_by_name(name)
+
+    if item is None:
+        raise HTTPException(404, f'Item with name {name} not found')
+
+    return item
